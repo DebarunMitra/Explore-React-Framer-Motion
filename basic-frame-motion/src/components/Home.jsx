7a59f993c21a6464270039572de7e5c34eb89b68
DebarunMitra <debarun96mitra@gmail.com>
2023-04-25 00:26:14 +0530

    variants added

--- basic-frame-motion/src/components/Home.jsx
+++ basic-frame-motion/src/components/Home.jsx
@@ -12,12 +12,22 @@ const Home = () => {
     animate={{
       opacity: 1
     }} 
+    transition={{
+      delay: 0.5,
+      duration: 2
+    }}
     className="home container">
       <motion.h2 animate={{}}>
         Welcome To Pizza Joint
       </motion.h2>
       <Link to="/base">
-        <motion.button animate={{}}>
+        <motion.button 
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgb(255, 255, 255)",
+            boxShadow: "0px 0px 8px rgb(255, 255, 255)"
+          }}
+        >
           Create Your Pizza
         </motion.button>
       </Link>
