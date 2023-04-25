71e13cc74426d55333be85b795706aa8891d1022
DebarunMitra <debarun96mitra@gmail.com>
2023-04-25 11:27:30 +0530

    Button variants scale keyframe added

--- basic-frame-motion/src/components/Toppings.jsx
+++ basic-frame-motion/src/components/Toppings.jsx
@@ -17,6 +17,19 @@ const containerVariants = {
   }
 }
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255, 255, 255)",
+    boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+    transition: {
+      duration: 0.3,
+      repeat: Infinity,
+      repeatType: 'reverse'
+    }
+  }
+}
+
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
 
@@ -43,14 +56,11 @@ const Toppings = ({ addTopping, pizza }) => {
       </ul>
 
       <Link to="/order">
-      <motion.button
-               whileHover={{
-                scale: 1.1,
-                textShadow: "0px 0px 8px rgb(255, 255, 255)",
-                boxShadow: "0px 0px 8px rgb(255, 255, 255)"
-              }}
-            >Order
-            </motion.button>
+        <motion.button
+            variants={buttonVariants}
+            whileHover="hover"
+          >Order
+        </motion.button>
       </Link>
 
     </motion.div>
