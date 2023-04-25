995f48b4cdd3d867cb4db9006afbca71eb8ea3d0
DebarunMitra <debarun96mitra@gmail.com>
2023-04-25 18:31:38 +0530

    page transition issue resolved for AnimatePresence

--- basic-frame-motion/src/components/Toppings.jsx
+++ basic-frame-motion/src/components/Toppings.jsx
@@ -10,10 +10,10 @@ const containerVariants = {
   visible: {
     opacity: 1,
     x: 0,
-    transition: {
-      type: 'spring',
-      delay: 0.5
-    }
+    // transition: {
+    //   type: 'spring',
+    //   delay: 0.5
+    // }
   },
   exit:{
     x: "-100vw",
